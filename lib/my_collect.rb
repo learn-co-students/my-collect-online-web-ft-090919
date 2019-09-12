8b861af3e12e99886bf3376aae1111fd878a25ca
Sarah H <sarahrose.henderson@gmail.com>
2019-09-12 14:39:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+counter = 0
+collection = []
+while counter < array.length
+collection <<  yield(array[counter])
+counter += 1
+end
+collection
+end
 
-
+my_collect(["Tim Jones", "John Pat", "Luna Lovegood"]) { |name| "Hi, my name is #{name}."}
